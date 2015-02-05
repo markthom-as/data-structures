6d67ba48a0138ba3aa7cba14c46a158dcc8853e8
mmarcant <m.marcantano@gmail.com>
2015-02-05 11:01:27 -0800

    implement graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,66 @@
-
-
 var Graph = function(){
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(node){
+  if ( this.nodes[node] === undefined) {
+    this.nodes[node] = null;
+  };
 };
 
 Graph.prototype.contains = function(node){
+  for ( var key in this.nodes ) {
+    if ( key === node ) {
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  var temp = this.nodes[node];
+  delete this.nodes[node];
+  return temp;
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  if ( this.nodes[fromNode][toNode] ) {
+    return true;
+  }
+  return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  if ( this.nodes[fromNode] === null ) {
+    this.nodes[fromNode] = {};
+  }
+  if ( this.nodes[toNode] === null ) {
+    this.nodes[toNode] = {};
+  }
+  this.nodes[fromNode][toNode] = true;
+  this.nodes[toNode][fromNode] = true;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  delete this.nodes[fromNode][toNode];
+  delete this.nodes[toNode][fromNode];
+  if ( Object.keys(this.nodes[fromNode]).length === 0) {
+    this.nodes[fromNode] = null;
+  }
+  if ( Object.keys(this.nodes[toNode]).length === 0) {
+    this.nodes[toNode] = null;
+  }
 };
 
 Graph.prototype.forEachNode = function(cb){
+  var list = this.nodes;
+  for ( var key in this.nodes ) {
+    cb(key);
+  }
+  console.log(this.nodes);
+  // _.each(list, function(value){
+  //   console.log(list);
+  //   cb(value)});
 };
 
 /*
@@ -29,4 +68,7 @@ Graph.prototype.forEachNode = function(cb){
  */
 
 
+  //a: {b: true,
+  //    c: true,
+  //    }
 
