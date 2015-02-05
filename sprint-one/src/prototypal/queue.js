ef313285de6eb04d53381dc6089a210c8598bf38
mmarcant <m.marcantano@gmail.com>
2015-02-04 16:10:31 -0800

    queue and stack for prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.sizeVar = 0;
+  queue.storage = {};
+  return queue;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this.storage[this.sizeVar] = value;
+  this.sizeVar++;
+  return this.sizeVar;
+}
+
+queueMethods.dequeue = function(){
+  var temp;
+  if(this.sizeVar > 0){
+    temp = this.storage[0];
+    delete this.storage[0];
+    this.sizeVar--;
+    var obj = {};
+    for(var key in this.storage){
+      obj[Number(key) - 1] = this.storage[key];
+    }
+    this.storage = obj;
+  }
+  return temp;
+}
+
+queueMethods.size = function(){
+  return this.sizeVar;
+}
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.sizeVar = 0;
+  stack.storage = {};
+  return stack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.sizeVar] = value;
+  this.sizeVar++;
+  return this.sizeVar;
+}
+
+stackMethods.pop = function() {
+  var temp;
+  if ( this.sizeVar > 0 ) {
+    temp = this.storage[this.sizeVar - 1];
+    delete this.storage[this.sizeVar - 1];
+    this.sizeVar--;
+  }
+  return temp;
+}
+
+stackMethods.size = function() {
+  return this.sizeVar;
+}
 
