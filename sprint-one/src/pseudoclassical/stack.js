dca15300a7cfb51cc6dde700b994158343891c75
mmarcant <m.marcantano@gmail.com>
2015-02-04 19:35:11 -0800

    Refactor code

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,8 +1,8 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-this.sizeVar = 0;
-this.storage = {};
+  this.sizeVar = 0;
+  this.storage = {};
 };
 
 Stack.prototype.push = function(value){
