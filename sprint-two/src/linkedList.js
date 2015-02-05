3c149f7a57c2242bd4cb57f30b67ac3608306f3c
mmarcant <m.marcantano@gmail.com>
2015-02-04 20:20:02 -0800

    implement linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,46 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if ( !this.tail) {
+      this.tail = Node(value);
+      this.head = this.tail;
+    } else if ( this.head === this.tail ) {
+      this.tail = Node(value);
+      this.head.next = this.tail;
+    } else {
+      this.tail.next = Node(value);
+      this.tail = this.tail.next;
+    }
   };
 
-  list.removeHead = function(){
+  list.removeHead = function() {
+    var temp;
+    if ( this.head !== null ) {
+      if ( this.head === this.tail ) {
+        temp = this.head.value;
+        this.head = null;
+        this.tail = null;
+      } else {
+        temp = this.head.value;
+        this.head = this.head.next;
+      }
+    }
+    return temp;
   };
 
   list.contains = function(target){
+    var node;
+    if ( this.head ) {
+      node = this.head;
+      while ( node ) {
+        if ( node.value === target ) {
+          return true;
+        } else {
+          node = node.next;
+        }
+      }
+    }
+    return false;
   };
 
   return list;
